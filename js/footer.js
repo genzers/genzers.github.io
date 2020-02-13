39020b7846bc90352ad4a5c7906b158701389e3c
Willy <crashingballoon@gmail.com>
2020-02-14 07:38:15 +0800

    add html js

---
+++ js/footer.js
@@ -0,0 +1,129 @@
+document.write(`
+	<footer class="footer">
+		<div class="footer-flex-container">
+			<div class="countdown_title_div">
+				<h1 class="countdown_title">TedX RongXing Garden <strong>Countdown</strong></h1>
+			</div>
+			<div class="countdown_wrap">
+				<div class="countdown">
+					<div class="bloc-time days" data-init-value="999">
+						<span class="count-title">Days</span>
+						<div class="figure days days-1">
+							<span class="top">9</span>
+							<span class="top-back">
+								<span>9</span>
+							</span>
+							<span class="bottom">9</span>
+							<span class="bottom-back">
+								<span>9</span>
+							</span>
+						</div>
+						<div class="figure days days-2">
+							<span class="top">9</span>
+							<span class="top-back">
+								<span>9</span>
+							</span>
+							<span class="bottom">9</span>
+							<span class="bottom-back">
+								<span>9</span>
+							</span>
+						</div>
+						<div class="figure days days-3">
+							<span class="top">9</span>
+							<span class="top-back">
+								<span>9</span>
+							</span>
+							<span class="bottom">9</span>
+							<span class="bottom-back">
+								<span>9</span>
+							</span>
+						</div>
+					</div>
+					<div class="bloc-time hours" data-init-value="99">
+						<span class="count-title">Hours</span>
+						<div class="figure hours hours-1">
+							<span class="top">9</span>
+							<span class="top-back">
+								<span>9</span>
+							</span>
+							<span class="bottom">9</span>
+							<span class="bottom-back">
+								<span>9</span>
+							</span>
+						</div>
+						<div class="figure hours hours-2">
+							<span class="top">9</span>
+							<span class="top-back">
+								<span>9</span>
+							</span>
+							<span class="bottom">9</span>
+							<span class="bottom-back">
+								<span>9</span>
+							</span>
+						</div>
+					</div>
+					<div class="bloc-time min" data-init-value="99">
+						<span class="count-title">Minutes</span>
+						<div class="figure min min-1">
+							<span class="top">9</span>
+							<span class="top-back">
+								<span>9</span>
+							</span>
+							<span class="bottom">9</span>
+							<span class="bottom-back">
+								<span>9</span>
+							</span>
+						</div>
+						<div class="figure min min-2">
+							<span class="top">9</span>
+							<span class="top-back">
+								<span>9</span>
+							</span>
+							<span class="bottom">9</span>
+							<span class="bottom-back">
+								<span>9</span>
+							</span>
+						</div>
+					</div>
+					<div class="bloc-time sec" data-init-value="99">
+						<span class="count-title">Seconds</span>
+						<div class="figure sec sec-1">
+							<span class="top">9</span>
+							<span class="top-back">
+								<span>9</span>
+							</span>
+							<span class="bottom">9</span>
+							<span class="bottom-back">
+								<span>9</span>
+							</span>
+						</div>
+						<div class="figure sec sec-2">
+							<span class="top">9</span>
+							<span class="top-back">
+								<span>9</span>
+							</span>
+							<span class="bottom">9</span>
+							<span class="bottom-back">
+								<span>9</span>
+							</span>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="foopic">
+				<a href="#"><img src="images/logo.png" alt="">
+				</a>
+				<p class="footer-content">Since 2019</p>
+				<div class="foo-social">
+					<a href="#">
+						<i class="fab fa-facebook-f" style="color: white"></i>
+					</a>
+					<a href="https://www.instagram.com/genz_are_us/?hl=zh-tw" target="_blank">
+						<i class="fab fa-instagram" style="color: white"></i>
+					</a>
+				</div>
+			</div>
+		</div>
+	</footer>
+`);
---
+++ js/header.js
@@ -0,0 +1,89 @@
+document.write(`
+	<header class="header banner">
+			<a href="../genzers.github.io/index.html" >
+				<img id="logoimg" src="../genzers.github.io/images/logo.png" alt="">
+			</a>
+		<div class="container">
+			<div class="row">
+				<div class="dropdown">
+					<nav class="menu">
+						<ul>
+							<li><a href="../genzers.github.io/index.html" style="font-size:20px;">Home</a>
+							</li>
+						</ul>
+
+						<div class="dropdown-content">
+						  <a href="../genzers.github.io/about_us.html">About Us</a>
+						  <a href="#">Follow Us</a>
+						  <a href="#">Testimony</a>
+						</div>
+					</nav>
+				</div>
+
+				<div class="dropdown">
+					<nav class="menu">
+						<ul>
+							<li><a href="../genzers.github.io/index.html" style="font-size:20px;" >About us</a>
+							</li>
+						</ul>
+						<div class="dropdown-content">
+						  <a href="../genzers.github.io/about_us.html">Registry</a>
+						  <a href="#">Team</a>
+						</div>
+					</nav>
+				</div>
+
+				<div class="dropdown">
+					<nav class="menu">
+						<ul>
+							<li><a href="../genzers.github.io/index.html" style="font-size:20px;" >events</a>
+							</li>
+						</ul>
+						<div class="dropdown-content">
+						  <a href="../genzers.github.io/about_us.html">Tedx</a>
+						</div>
+					</nav>
+				</div>
+
+				<div class="dropdown">
+					<nav class="menu">
+						<ul>
+							<li><a href="../genzers.github.io/subwebsites/stories/stories.html" style="font-size:20px;" >stories</a>
+							</li>
+						</ul>
+						<div class="dropdown-content">
+						  <a href="../genzers.github.io/about_us.html" style="font-size:15px;">無框</a>
+						  <a href="#" style="font-size:15px;">連結</a>
+						  <a href="#" style="font-size:15px;">共同</a>
+						</div>
+					</nav>
+				</div>
+
+				<div class="dropdown">
+					<nav class="menu">
+						<ul>
+							<li><a href="../genzers.github.io/index.html" style="font-size:20px;" >contact us</a>
+							</li>
+						</ul>
+						<div class="dropdown-content">
+						  <a href="../genzers.github.io/about_us.html">faq</a>
+						  <a href="#">rsvp</a>
+						</div>
+					</nav>
+				</div>
+
+				<div class="dropdown">
+					<nav class="menu">
+						<ul>
+							<li><a href="../genzers.github.io/index.html" style="font-size:20px;" >store</a>
+							</li>
+						</ul>
+					</nav>
+				</div>
+			</div>
+		</div>
+	</header>
+
+
+
+`);
