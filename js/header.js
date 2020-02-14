5e7b097e5ea02c83f6b473f14afe0d89363546fd
Willy <crashingballoon@gmail.com>
2020-02-14 11:21:50 +0800

    change to specifi address

--- js/header.js
+++ js/header.js
@@ -1,19 +1,19 @@
 document.write(`
 	<header class="header banner">
-			<a href="../genzers.github.io/index.html" >
-				<img id="logoimg" src="../genzers.github.io/images/logo.png" alt="">
+			<a href="http://genzers.nctu.me/genzers.github.io/index.html" >
+				<img id="logoimg" src="http://genzers.nctu.me/genzers.github.io/images/logo.png" alt="">
 			</a>
 		<div class="container">
 			<div class="row">
 				<div class="dropdown">
 					<nav class="menu">
 						<ul>
-							<li><a href="../genzers.github.io/index.html" style="font-size:20px;">Home</a>
+							<li><a href="http://genzers.nctu.me/genzers.github.io/index.html" style="font-size:20px;">Home</a>
 							</li>
 						</ul>
 
 						<div class="dropdown-content">
-						  <a href="../genzers.github.io/about_us.html">About Us</a>
+						  <a href="http://genzers.nctu.me/genzers.github.io/about_us.html">About Us</a>
 						  <a href="#">Follow Us</a>
 						  <a href="#">Testimony</a>
 						</div>
@@ -23,11 +23,11 @@ document.write(`
 				<div class="dropdown">
 					<nav class="menu">
 						<ul>
-							<li><a href="../genzers.github.io/index.html" style="font-size:20px;" >About us</a>
+							<li><a href="http://genzers.nctu.me/genzers.github.io/index.html" style="font-size:20px;" >About us</a>
 							</li>
 						</ul>
 						<div class="dropdown-content">
-						  <a href="../genzers.github.io/about_us.html">Registry</a>
+						  <a href="http://genzers.nctu.me/genzers.github.io/about_us.html">Registry</a>
 						  <a href="#">Team</a>
 						</div>
 					</nav>
@@ -36,11 +36,11 @@ document.write(`
 				<div class="dropdown">
 					<nav class="menu">
 						<ul>
-							<li><a href="../genzers.github.io/index.html" style="font-size:20px;" >events</a>
+							<li><a href="http://genzers.nctu.me/genzers.github.io/index.html" style="font-size:20px;" >events</a>
 							</li>
 						</ul>
 						<div class="dropdown-content">
-						  <a href="./subwebsites/events/Tedx/Tedx.html">Tedx</a>
+						  <a href="http://genzers.nctu.me/genzers.github.io/subwebsites/events/Tedx/Tedx.html">Tedx</a>
 						</div>
 					</nav>
 				</div>
@@ -48,11 +48,11 @@ document.write(`
 				<div class="dropdown">
 					<nav class="menu">
 						<ul>
-							<li><a href="../genzers.github.io/subwebsites/stories/stories.html" style="font-size:20px;" >stories</a>
+							<li><a href="http://genzers.nctu.me/genzers.github.io/subwebsites/stories/stories.html" style="font-size:20px;" >stories</a>
 							</li>
 						</ul>
 						<div class="dropdown-content">
-						  <a href="../genzers.github.io/about_us.html" style="font-size:15px;">無框</a>
+						  <a href="http://genzers.nctu.me/genzers.github.io/subwebsites/stories/無框/無框.html" style="font-size:15px;">無框</a>
 						  <a href="#" style="font-size:15px;">連結</a>
 						  <a href="#" style="font-size:15px;">共同</a>
 						</div>
@@ -62,11 +62,11 @@ document.write(`
 				<div class="dropdown">
 					<nav class="menu">
 						<ul>
-							<li><a href="../genzers.github.io/index.html" style="font-size:20px;" >contact us</a>
+							<li><a href="http://genzers.nctu.me/genzers.github.io/index.html" style="font-size:20px;" >contact us</a>
 							</li>
 						</ul>
 						<div class="dropdown-content">
-						  <a href="../genzers.github.io/about_us.html">faq</a>
+						  <a href="http://genzers.nctu.me/genzers.github.io/about_us.html">faq</a>
 						  <a href="#">rsvp</a>
 						</div>
 					</nav>
@@ -75,7 +75,7 @@ document.write(`
 				<div class="dropdown">
 					<nav class="menu">
 						<ul>
-							<li><a href="../genzers.github.io/index.html" style="font-size:20px;" >store</a>
+							<li><a href="http://genzers.nctu.me/genzers.github.io/index.html" style="font-size:20px;" >store</a>
 							</li>
 						</ul>
 					</nav>
