7f6bd8767432178ecb96a92dc9955ec403fdc705
Willy <crashingballoon@gmail.com>
2020-02-28 03:19:09 +0800

    animation scroll down -10

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ function autoScrollTo(el) {
 	if(yPos > bodyHeight){
 		clearTimeout(animator);
 	} else {
-		if(currentY < targetY-distance){
+		if(currentY < targetY-distance -10){
 			var scrollY = currentY+distance;
 			window.scroll(0, scrollY);
 		} else {
