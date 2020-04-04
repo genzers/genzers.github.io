6ad8c59f9fa3141611b5d2e1c1d4b5fb6b146851
magician3124 <crashingballoon@gmail.com>
2020-04-04 08:43:05 +0800

    fix dropdown link

---
+++ js/header_for_others.js
@@ -0,0 +1,114 @@
+document.write(`
+	<header class="header banner">
+			<a href="http://genzers.nctu.me/genzers.github.io/index.html" >
+				<img id="logoimg" src="http://genzers.nctu.me/genzers.github.io/images/logo.png" alt="">
+			</a>
+		<div class="container">
+			<div class="row">
+				<div class="dropdown">
+					<nav class="menu">
+						<ul>
+							<li><a href="http://genzers.nctu.me/genzers.github.io/index.html" style="font-size:20px;">Home</a>
+							</li>
+						</ul>
+
+						<div class="dropdown-content">
+						  <a href="http://genzers.nctu.me/genzers.github.io/index.html">About Us</a>
+						  <a href="http://genzers.nctu.me/genzers.github.io/index.html">Contact Us</a>
+						  <a href="#">Testimony</a>
+						</div>
+					</nav>
+				</div>
+
+				<div class="dropdown">
+					<nav class="menu">
+						<ul>
+							<li><a href="http://genzers.nctu.me/genzers.github.io/subwebsites/team/team.html" style="font-size:20px;" >Team</a>
+							</li>
+						</ul>
+					</nav>
+				</div>
+
+				<div class="dropdown">
+					<nav class="menu">
+						<ul>
+							<li><a href="http://genzers.nctu.me/genzers.github.io/subwebsites/events/events.html" style="font-size:20px;" >events</a>
+							</li>
+						</ul>
+						<div class="dropdown-content">
+						  <a href="http://genzers.nctu.me/genzers.github.io/subwebsites/events/Tedx/Tedx.html">Tedx</a>
+						</div>
+					</nav>
+				</div>
+
+				<div class="dropdown">
+					<nav class="menu">
+						<ul>
+							<li><a href="http://genzers.nctu.me/genzers.github.io/subwebsites/stories/stories.html" style="font-size:20px;" >stories</a>
+							</li>
+						</ul>
+						<div class="dropdown-content">
+						  <a href="http://genzers.nctu.me/genzers.github.io/subwebsites/stories/無框/無框.html" style="font-size:15px;">無框</a>
+						  <a href="#" style="font-size:15px;">連結</a>
+						  <a href="#" style="font-size:15px;">共同</a>
+						</div>
+					</nav>
+				</div>
+
+				<div class="dropdown">
+					<nav class="menu">
+						<ul>
+							<li><a href="http://genzers.nctu.me/genzers.github.io/index.html" style="font-size:20px;" >contact us</a>
+							</li>
+						</ul>
+						<div class="dropdown-content">
+						  <a href="http://genzers.nctu.me/genzers.github.io/about_us.html">faq</a>
+						  <a href="#">rsvp</a>
+						</div>
+					</nav>
+				</div>
+				<!--
+				<div class="dropdown">
+					<nav class="menu">
+						<ul>
+							<li><a href="http://genzers.nctu.me/genzers.github.io/index.html" style="font-size:20px;" >store</a>
+							</li>
+						</ul>
+					</nav>
+				</div>
+
+				<div class="dropdown">
+					<nav class="menu">
+						<ul>
+							<li><a href="http://genzers.nctu.me/genzers.github.io/subwebsites/sponsors/sponsors.html" style="font-size:20px;">Sponsors</a>
+							</li>
+						</ul>
+					</nav>
+				</div>
+				-->
+				<div class="container" id="phonecontainer">
+					<div class="row">
+						<div class="phonetitle">
+							<h>Generation Z</h>
+						</div>
+						<div class="dropdown" id="res">
+						<nav class="menu">
+							<ul>
+								<li><a class="icon">&#9776;</a>
+								</li>
+							</ul>
+							<div class="dropdown-content-mobile" style="right: 0;">
+								<a href="http://genzers.nctu.me/genzers.github.io/index.html" style="font-size:20px;">home</a>
+								<a href="http://genzers.nctu.me/genzers.github.io/subwebsites/team/team.html" style="font-size:20px;">team</a>
+								<a href="http://genzers.nctu.me/genzers.github.io/subwebsites/events/events.html" style="font-size:20px;">events</a>
+								<a href="http://genzers.nctu.me/genzers.github.io/subwebsites/stories/stories.html" style="font-size:20px;">stories</a>
+								<a href="#" style="font-size:20px;">contact us</a>
+							</div>
+						</nav>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+`);
