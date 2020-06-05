e5ae1887720fa86f414e5c1cc68a0725aa65208a
Exitialium <henrywu0424@gmail.com>
2020-06-05 17:09:33 +0800

    team finish

---
+++ subwebsites/team/subwebsites/js/script.js
@@ -0,0 +1,18 @@
+var distance = 40;
+function autoScrollTo(el) {
+	var currentY = window.pageYOffset;
+	var targetY = document.getElementById(el).offsetTop;
+	var bodyHeight = document.body.offsetHeight;
+	var yPos = currentY + window.innerHeight;
+	var animator = setTimeout('autoScrollTo(\''+el+'\')',24);
+	if(yPos > bodyHeight){
+		clearTimeout(animator);
+	} else {
+		if(currentY < targetY-distance+650){
+			var scrollY = currentY+distance;
+			window.scroll(0, scrollY);
+		} else {
+			clearTimeout(animator);
+		}
+	}
+}
